2cae245da9d9ad805adf3529e703c5f2abe98457
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-11-06 16:35:16 +0000

    Size of Data Types

---
+++ C programing/size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("Size of Integer is:%d\n",sizeof(a));
+    printf("Size of Float is:%d\n",sizeof(b));
+    printf("Size of Character is:%d\n",sizeof(c));
+    printf("Size of Double is:%d\n",sizeof(d));
+    return 0;
+}
