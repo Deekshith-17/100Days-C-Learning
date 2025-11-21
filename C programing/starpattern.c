81cd03dc813a754a871c6d8697e4040ef318d249
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-11-21 16:11:48 +0000

    Print Star Pattern Using Loops and Conditional Statements

---
+++ C programing/starpattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int row, i, j;
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+
+    for (i = 1; i <= row; i++) {
+        for (j = 1; j <= i; j++) {
+            if (i == 1 || j == i || i == row)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
