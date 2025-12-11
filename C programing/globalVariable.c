8d98aff5185e147e0c43be979022f16017471eb6
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2025-12-11 09:59:47 +0000

    Global variable declaration

---
+++ C programing/globalVariable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Global variable declaration
+int globalVariable = 10;   // Assigning a value for demonstration
+
+int main()
+{
+    printf("The value of the global variable is: %d\n", globalVariable);
+    return 0;
+}
