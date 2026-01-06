6ef253803662883eeddf0ef33c17e0239d3009f8
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-01-06 09:22:40 +0000

    Integer Promotions

---
+++ C programing/Int Prom.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+   char a = 251;
+   unsigned char b = a;
+
+   printf("a = %c", a);
+   printf("\nb = %c", b);
+
+   if (a == b)
+      printf("\n Same");
+   else
+      printf("\n Not Same");
+   
+   return 0;
+}
