fd3f7737e1afaabe7ba68d97a0cae5ea1b716136
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-01-11 09:03:59 +0000

    Increment and Decrement Operators on Characters

---
+++ C programing/Inc and Dec.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char ch = 'A';
+    printf("Original character: %c\n", ch);
+
+    ch++; // Increment
+    printf("After increment: %c\n", ch);
+
+    ch--; // Decrement
+    printf("After decrement: %c\n", ch);
+    return 0;
+}
