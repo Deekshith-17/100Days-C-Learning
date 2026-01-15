3c6f48c6c5a02e4bd192c83586e46ba98e04f9a4
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-01-15 11:05:06 +0000

    Explicit Type Conversion

---
+++ C programing/Explicit.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+
+// int main(){
+
+//    int x = 10, y = 4;
+//    float z = x/y;
+
+//    printf("%f", z);
+
+//    return 0;
+// }
+
+
+#include <stdio.h>
+
+int main(){
+
+   int x = 10, y = 4;
+   float z = (float)x/y;
+
+   printf("%f", z);
+
+   return 0;
+}
