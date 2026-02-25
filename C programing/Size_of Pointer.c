cc418351d64f73ce8e3e280f9c85415d289d0485
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-02-25 09:46:34 +0000

    The Size of a Pointer

---
+++ C programing/Size_of Pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+   printf("Size of int data type: %d \n", sizeof(int *));
+   printf("Size of char data type: %d \n", sizeof(char *));
+   printf("Size of float data type: %d \n", sizeof(float *));
+   printf("Size of double data type: %d \n", sizeof(double *));
+   printf("Size of double pointer type: %d \n", sizeof(int **));
+}
