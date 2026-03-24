6ed6618c8b0bbe8191b31dec72ecedd250346eed
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-03-24 07:50:52 +0000

    Loops

---
+++ C programing/Loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+
+   // local variable definition
+   int a = 1;
+   
+   printf("a: %d\n", a);
+   a++;
+   
+   printf("a: %d\n", a);
+   a++;
+   
+   printf("a: %d\n", a);
+   a++;
+   
+   printf("a: %d\n", a);
+   a++;
+   
+   printf("a: %d\n", a);
+ 
+   return 0;
+}
