26aaf3c33e4c82c8739af12c7ae0ab7a071479d8
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-04-08 10:59:44 +0000

    Equate Two Variables

---
+++ C programing/Equate Two Var.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+   // local variable definition
+   int a = 10, b = 0;
+
+   // while loop execution
+   while(a != b){
+      a--;
+      b++;
+      printf("a: %d b: %d\n", a,b);
+   }
+   printf("\n End of loop");
+   return 0;
+}
