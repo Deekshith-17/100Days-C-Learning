ab6993960e4793df2dc4eeaed761f64f4cb825a5
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-04-12 16:09:36 +0000

    Take Input Until User Enters 0

---
+++ C programing/Take Input Until Enters 0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   int num;
+   printf("Enter numbers (0 to stop):\n");
+
+   scanf("%d", &num);
+   while (num != 0) {
+      printf("You entered: %d\n", num);
+      scanf("%d", &num);
+   }
+
+   return 0;
+}
