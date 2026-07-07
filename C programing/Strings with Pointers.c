beedbbfcfc0d2c1b87c8eb9e57464c2aa14add77
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-07-07 08:28:17 +0000

    An Array of Strings with Pointers

---
+++ C programing/Strings with Pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+   char *langs[10] = {
+      "PYTHON", "JAVASCRIPT", "PHP",
+      "NODE JS", "HTML", "KOTLIN", "C++", 
+      "REACT JS", "RUST", "VBSCRIPT"
+   };
+
+   for (int i = 0; i < 10; i++)
+      printf("%s\n", langs[i]);
+
+   return 0;
+}
