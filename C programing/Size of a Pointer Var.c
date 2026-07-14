dc37c82ab260aeaf42fea545b71229769b780227
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-07-14 16:45:10 +0000

    Size of a Pointer Variable

---
+++ C programing/Size of a Pointer Var.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int x = 10;
+  float y = 1.3f;
+  char z = 'p';
+
+  // Pointer declaration and initialization
+  int * ptr_x = & x;
+  float * ptr_y = & y;
+  char * ptr_z = & z;
+
+  // Printing the size of pointer variables
+  printf("Size of integer pointer : %lu\n", sizeof(ptr_x));
+  printf("Size of float pointer : %lu\n", sizeof(ptr_y));
+  printf("Size of char pointer : %lu\n", sizeof(ptr_z));
+  
+  return 0;
+}
