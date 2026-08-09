bac8ab9f967adcf955d02e76deace0dfd38d51fc
Deekshith Kumar <deekshith.kumar@s.amity.edu>
2026-08-09 11:41:35 +0000

    Passing an Array to Function

---
+++ C programing/Passing an Array to Fun.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int max(int *arr, int length);
+
+int main(){
+    
+   int arr[] = {10, 34, 21, 78, 5};
+   int length = sizeof(arr)/sizeof(int);
+
+   int maxnum = max(arr, length);
+      printf("max: %d", maxnum);
+}
+
+int max(int *arr, int length){
+
+   int max = *arr;
+        
+   for (int i = 0; i < length; i++){   
+      printf("arr[%d]: %d\n", i, (*arr));
+        
+      if ((*arr)>max)
+         max = (*arr);
+      arr++;
+   }
+   return max;
+}
